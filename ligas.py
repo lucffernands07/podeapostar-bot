b67f39c06c206d383cec273c6aa3a4f6f467b85b
lucffernands07 <lucianoffernands@outlook.com.br>
2026-04-26 07:59:12 -0300

    Add Andorra - Primeira Divisão link to ligas.py

--- ligas.py
+++ ligas.py
@@ -35,6 +35,7 @@ COMPETICOES = {
     # --- BLOCO 5: LIGAS ALTERNATIVAS E DIVISÕES DE ACESSO ---
     "México - Liga MX": "https://www.flashscore.com.br/futebol/mexico/liga-mx/jogos/",
     "Albânia - Super Liga": "https://www.flashscore.com.br/futebol/albania/super-liga/",
+    "Andorra - Primeira Divisão": "https://www.flashscore.com.br/futebol/andorra/primeira-divisao/",
     "Espanha - La Liga 2": "https://www.flashscore.com.br/futebol/espanha/laliga2/jogos/",
     "Alemanha - 2. Bundesliga": "https://www.flashscore.com.br/futebol/alemanha/2-bundesliga/jogos/",
     "Itália - Serie B": "https://www.flashscore.com.br/futebol/italia/serie-b/jogos/",
