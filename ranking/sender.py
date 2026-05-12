bd66774d2e355dc9ccb2b1b785a0e5276bed24a0
lucffernands07 <lucianoffernands@outlook.com.br>
2026-05-12 09:44:47 -0300

    Update empty square emoji to include variation selector

--- ranking/sender.py
+++ ranking/sender.py
@@ -9,7 +9,7 @@ def get_barra_progresso(percentual):
     blocos = int(percentual / 20)
     # Copie exatamente estes caracteres:
     quadrado_cheio = "🟩" # U+1F7E9
-    quadrado_vazio = "⬜" # U+2B1C
+    quadrado_vazio = "⬜️" # U+2B1C
     return (quadrado_cheio * blocos) + (quadrado_vazio * (5 - blocos))
 
 def gerar_tabela_ranking():
