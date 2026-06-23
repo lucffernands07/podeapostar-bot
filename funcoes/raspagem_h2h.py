e7da36e87cec93707525e8f2ea5b13c9dd78fac6
lucffernands07 <lucianoffernands@outlook.com.br>
2026-06-23 15:25:51 -0300

    Add import for links module in raspagem_h2h.py

--- funcoes/raspagem_h2h.py
+++ funcoes/raspagem_h2h.py
@@ -1,5 +1,6 @@
 import time
 import re
+import links
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
