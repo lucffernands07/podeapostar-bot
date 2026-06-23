320d2e27cdd1a1dcde18d6b1dbd4482567da4ec8
lucffernands07 <lucianoffernands@outlook.com.br>
2026-06-23 15:22:44 -0300

    Update raspagem_scout.py

--- funcoes/raspagem_scout.py
+++ funcoes/raspagem_scout.py
@@ -1,3 +1,8 @@
+import time
+import re
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def pegar_scouts_avancados(driver, stats, t1, t2):
     """
