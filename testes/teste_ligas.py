3d81fceaa63a95648f2c782c7b7222eee5f9750f
lucffernands07 <lucianoffernands@outlook.com.br>
2026-06-25 17:14:07 -0300

    Update Copa do Mundo URL to Campeonato do Mundo

--- testes/teste_ligas.py
+++ testes/teste_ligas.py
@@ -1,5 +1,5 @@
 # testes/teste_ligas.py
 
 TESTE_COMPETICOES = {
-    "Copa do Mundo": "https://www.flashscore.com.br/futebol/mundo/copa-do-mundo/jogos/"
+    "Mundo - Copa do Mundo": "https://www.flashscore.com.br/futebol/mundo/campeonato-do-mundo/jogos/",
 }
