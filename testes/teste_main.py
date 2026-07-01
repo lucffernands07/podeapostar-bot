a7ad9cf7143551dd1315fa0a2d5dd51e60e92d6f
lucffernands07 <lucianoffernands@outlook.com.br>
2026-07-01 11:15:14 -0300

    Update teste_main.py

--- testes/teste_main.py
+++ testes/teste_main.py
@@ -31,7 +31,7 @@ def enviar_telegram(mensagem, chat_id_destino):
     try:
         requests.post(url, data={
             "chat_id": chat_id_destino, 
-            "text": message,
+            "text": mensagem,  # 🎯 Corrigido aqui: mudado de message para mensagem
             "parse_mode": "Markdown",
             "disable_web_page_preview": True
         })
