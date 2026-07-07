854eb768d3db185e38aa0472c7bf48c76008e725
lucffernands07 <lucianoffernands@outlook.com.br>
2026-07-07 12:12:29 -0300

    Rename LIGAS_ELITE_CARTOES to LIGAS_ELITE_JOGADORES

--- mercados/cartoes.py
+++ mercados/cartoes.py
@@ -1,10 +1,10 @@
 import re
 
 # 🟢 LISTA BRANCA: Apenas ligas de elite que comprovadamente abrem mercados de jogadores na Betano
-LIGAS_ELITE_CARTOES = [
+LIGAS_ELITE_JOGADORES = [
     "Brasileirão Série A", "Copa do Brasil", "Libertadores", "Sul-Americana",
     "Brasileirão Série B", "Argentina - Liga Profesional", "Mundo - Copa do Mundo",
-    "Champions League", "Inglaterra - Premier League", "Espanha - LaLiga",
+    "Europa - Champions League", "Inglaterra - Premier League", "Espanha - LaLiga",
     "Alemanha - Bundesliga", "Italia - Serie A", "França - Ligue 1",
     "Europa - League", "Inglaterra - FA Cup", "Espanha - Copa del Rey",
     "Alemanha - DFB Pokal", "Portugal - Primeira Liga", "Países Baixos - Eredivisie",
