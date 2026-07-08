2d784f1b7a3ca8718d98d75e40e6eb77d7f32d56
lucffernands07 <lucianoffernands@outlook.com.br>
2026-07-08 13:32:37 -0300

    Update import statements for test modules

--- testes/teste_main.py
+++ testes/teste_main.py
@@ -14,8 +14,8 @@ from testes.teste_ligas import TESTE_COMPETICOES as COMPETICOES
 from testes.teste_jogadores import LIGAS_ELITE_JOGADORES
 from testes.teste_cartoes import LIGAS_ELITE_JOGADORES
 from mercados import gols, ambos_marcam, chance_dupla, vitoria_casa
-from testes.teste_cartoes as cartoes
-from testes.teste_jogadores as jogadores
+from testes import teste_cartoes as cartoes
+from testes import teste_jogadores as jogadores
 import odds, bingo357
 from telegram import menus
 
