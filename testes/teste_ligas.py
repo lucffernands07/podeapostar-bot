10b9a872930969af6d0b9a3c9aaeced84e26d13d
lucffernands07 <lucianoffernands@outlook.com.br>
2026-07-08 13:23:53 -0300

    Update TESTE_COMPETICOES with Champions League URL

--- testes/teste_ligas.py
+++ testes/teste_ligas.py
@@ -1,5 +1,5 @@
 # testes/teste_ligas.py
 
 TESTE_COMPETICOES = {
-    "Mundo - Copa do Mundo": "https://www.flashscore.com.br/futebol/mundo/campeonato-do-mundo/jogos/",
+    "Europa - Champions League": "https://www.flashscore.com.br/futebol/europa/liga-dos-campeoes/jogos/",
 }
