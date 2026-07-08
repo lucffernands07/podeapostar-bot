97caed99854f88ecd56ea3e09d419d7104b4697d
lucffernands07 <lucianoffernands@outlook.com.br>
2026-07-08 14:34:00 -0300

    Add World Cup URL to TESTE_COMPETICOES

--- testes/teste_ligas.py
+++ testes/teste_ligas.py
@@ -2,4 +2,5 @@
 
 TESTE_COMPETICOES = {
     "Europa - Champions League": "https://www.flashscore.com.br/futebol/europa/liga-dos-campeoes/jogos/",
+    "Mundo - Copa do Mundo": "https://www.flashscore.com.br/futebol/mundo/campeonato-do-mundo/jogos/",
 }
