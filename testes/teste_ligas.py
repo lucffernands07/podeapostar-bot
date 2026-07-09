c7b07128f8393652a1939c51e982550d892eb316
lucffernands07 <lucianoffernands@outlook.com.br>
2026-07-09 17:22:25 -0300

    Fix formatting and add missing comma in TESTE_COMPETICOES

--- testes/teste_ligas.py
+++ testes/teste_ligas.py
@@ -2,6 +2,6 @@
 
 TESTE_COMPETICOES = {
     #"Europa - Champions League": "https://www.flashscore.com.br/futebol/europa/liga-dos-campeoes/jogos/",
-    "Mundo - Copa do Mundo": "https://www.flashscore.com.br/futebol/mundo/campeonato-do-mundo/jogos/"
-    #"Brasileirão Série B": "https://www.flashscore.com.br/futebol/brasil/brasileirao-serie-b/jogos/"
+    "Mundo - Copa do Mundo": "https://www.flashscore.com.br/futebol/mundo/campeonato-do-mundo/jogos/",
+    "Brasileirão Série B": "https://www.flashscore.com.br/futebol/brasil/brasileirao-serie-b/jogos/"
 }
