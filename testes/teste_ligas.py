a95157e33633fecdb82a477c34ddc8891e407aea
lucffernands07 <lucianoffernands@outlook.com.br>
2026-07-09 15:18:21 -0300

    Comment out competition URLs in teste_ligas.py
    Comment out some competition URLs in the TESTE_COMPETICOES dictionary.

--- testes/teste_ligas.py
+++ testes/teste_ligas.py
@@ -1,7 +1,7 @@
 # testes/teste_ligas.py
 
 TESTE_COMPETICOES = {
-    "Europa - Champions League": "https://www.flashscore.com.br/futebol/europa/liga-dos-campeoes/jogos/",
-    "Mundo - Copa do Mundo": "https://www.flashscore.com.br/futebol/mundo/campeonato-do-mundo/jogos/",
-    "Brasileirão Série B": "https://www.flashscore.com.br/futebol/brasil/brasileirao-serie-b/jogos/"
+    #"Europa - Champions League": "https://www.flashscore.com.br/futebol/europa/liga-dos-campeoes/jogos/",
+    "Mundo - Copa do Mundo": "https://www.flashscore.com.br/futebol/mundo/campeonato-do-mundo/jogos/"
+    #"Brasileirão Série B": "https://www.flashscore.com.br/futebol/brasil/brasileirao-serie-b/jogos/"
 }
