f15a8d9f44eb2c63079a995dc5847eec96254d48
lucffernands07 <lucianoffernands@outlook.com.br>
2026-07-11 16:25:54 -0300

    Update teste_ligas.py

--- testes/teste_ligas.py
+++ testes/teste_ligas.py
@@ -1,8 +1,8 @@
 # testes/teste_ligas.py
 
 TESTE_COMPETICOES = {
-    "Europa - Champions League": "https://www.flashscore.com.br/futebol/europa/liga-dos-campeoes/jogos/",
-    "Mundo - Copa do Mundo": "https://www.flashscore.com.br/futebol/mundo/campeonato-do-mundo/jogos/",
-    "Brasileirão Série B": "https://www.flashscore.com.br/futebol/brasil/brasileirao-serie-b/jogos/",
-    "Argentina - Primeira B": "https://www.flashscore.com.br/futebol/argentina/primera-b/jogos/"
+    #"Europa - Champions League": "https://www.flashscore.com.br/futebol/europa/liga-dos-campeoes/jogos/",
+    "Mundo - Copa do Mundo": "https://www.flashscore.com.br/futebol/mundo/campeonato-do-mundo/jogos/"
+    #"Brasileirão Série B": "https://www.flashscore.com.br/futebol/brasil/brasileirao-serie-b/jogos/",
+    #"Argentina - Primeira B": "https://www.flashscore.com.br/futebol/argentina/primera-b/jogos/"
 }
