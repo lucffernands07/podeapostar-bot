20e9ad430ab970f03677260eb61eb33101007c43
lucffernands07 <lucianoffernands@outlook.com.br>
2026-07-13 21:51:13 -0300

    Update competition URLs in teste_ligas.py
    Rearranged the competition URLs in the TESTE_COMPETICOES dictionary, commenting out some entries and adding others.

--- testes/teste_ligas.py
+++ testes/teste_ligas.py
@@ -2,8 +2,8 @@
 
 TESTE_COMPETICOES = {
     #"Europa - Champions League": "https://www.flashscore.com.br/futebol/europa/liga-dos-campeoes/jogos/",
-    #"Mundo - Copa do Mundo": "https://www.flashscore.com.br/futebol/mundo/campeonato-do-mundo/jogos/",
-    "Brasileirão Série B": "https://www.flashscore.com.br/futebol/brasil/brasileirao-serie-b/jogos/",
-    "Brasileiro Serie C": "https://www.flashscore.com.br/futebol/brasil/serie-c/jogos/",
-    "Brasileiro Serie D": "https://www.flashscore.com.br/futebol/brasil/serie-d/jogos/"
+    "Mundo - Copa do Mundo": "https://www.flashscore.com.br/futebol/mundo/campeonato-do-mundo/jogos/"
+    #"Brasileirão Série B": "https://www.flashscore.com.br/futebol/brasil/brasileirao-serie-b/jogos/",
+    #"Brasileiro Serie C": "https://www.flashscore.com.br/futebol/brasil/serie-c/jogos/",
+    #"Brasileiro Serie D": "https://www.flashscore.com.br/futebol/brasil/serie-d/jogos/"
 }
