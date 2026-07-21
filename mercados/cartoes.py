e86aaedd0b4104f7b9c8c3b5498c5e9dfe43501f
lucffernands07 <lucianoffernands@outlook.com.br>
2026-07-21 09:39:20 -0300

    Change import statement for jogadores module

--- mercados/cartoes.py
+++ mercados/cartoes.py
@@ -1,5 +1,5 @@
 import re
-import jogadores  # Importa o módulo onde fica a trava de validação de liga elite
+from . import jogadores  # Importa o módulo onde fica a trava de validação de liga elite
 
 def analisar_dados_cartoes(cartoes_mandante_h2h, cartoes_visitante_h2h, nome_liga="", quantidade_jogos=3):
     """
