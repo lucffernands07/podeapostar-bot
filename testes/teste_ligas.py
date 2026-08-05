92d535c4d570be7466442d488a34153b45901f4d
lucffernands07 <lucianoffernands@outlook.com.br>
2026-08-05 18:54:53 -0300

    Rename TESTE_COMPETICOES to COMPETICOES

--- testes/teste_ligas.py
+++ testes/teste_ligas.py
@@ -1,8 +1,9 @@
 # testes/teste_ligas.py
 
-TESTE_COMPETICOES = {
+COMPETICOES = {
     #"Europa - Champions League": "https://www.flashscore.com.br/futebol/europa/liga-dos-campeoes/jogos/",
-    "Mundo - Copa do Mundo": "https://www.flashscore.com.br/futebol/mundo/campeonato-do-mundo/jogos/"
+    #"Mundo - Copa do Mundo": "https://www.flashscore.com.br/futebol/mundo/campeonato-do-mundo/jogos/"
+    "Copa do Brasil": "https://www.flashscore.com.br/futebol/brasil/copa-betano-do-brasil/jogos/",
     #"Brasileirão Série B": "https://www.flashscore.com.br/futebol/brasil/brasileirao-serie-b/jogos/",
     #"Brasileiro Serie C": "https://www.flashscore.com.br/futebol/brasil/serie-c/jogos/",
     #"Brasileiro Serie D": "https://www.flashscore.com.br/futebol/brasil/serie-d/jogos/"
