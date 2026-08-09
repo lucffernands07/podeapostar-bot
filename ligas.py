00f91fe2fb49c5233b5f7245b43f96f029bab30c
lucffernands07 <lucianoffernands@outlook.com.br>
2026-08-09 09:45:32 -0300

    Comment out Kazakhstan Liga URL in ligas.py

--- ligas.py
+++ ligas.py
@@ -36,7 +36,7 @@ COMPETICOES = {
     "Copa da Turquia": "https://www.flashscore.com.br/futebol/turquia/copa-da-turquia/jogos/",
     "Áustria - Bundesliga": "https://www.flashscore.com.br/futebol/austria/bundesliga/jogos/",
     "Dinamarca - Superliga": "https://www.flashscore.com.br/futebol/dinamarca/superliga/jogos/",
-    "Cazaquistão - Primeira Liga": "https://www.flashscore.com.br/futebol/cazaquistao/primeira-liga/jogos",
+    #"Cazaquistão - Primeira Liga": "https://www.flashscore.com.br/futebol/cazaquistao/primeira-liga/jogos",
 
     # --- BLOCO 5: LIGAS E COPAS ALTERNATIVAS E DIVISÕES DE ACESSO ---
     "Equador - Copa": "https://www.flashscore.com.br/futebol/equador/copa-do-equador/jogos/",
